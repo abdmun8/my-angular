eb2472b9f8bbdd238ed9e79d57164c575d9b7a1d
Abdul Munim <abdmun8@gmail.com>
2022-01-15 06:43:37 +0700

    feat: add module auth & admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  title: String = 'my-app';
+  currentPath: String = window.location.pathname;
+
+  ngOnInit() {
+    console.log(this.currentPath);
+  }
+  ngonUpdate() {
+    this.currentPath = window.location.pathname;
+  }
+
 }
